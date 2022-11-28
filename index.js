0dafd792de3dc66a0782909bce75d5aae70df1a3
Programmer Bikash <bikash19081998dey@gmail.com>
2022-11-28 09:55:03 +0600

    is Seller Check

--- index.js
+++ index.js
@@ -148,6 +148,13 @@ async function run() {
             res.send({ isAdmin: user?.role === 'admin' });
         })
 
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.category === 'Seller' });
+        })
+
          app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
